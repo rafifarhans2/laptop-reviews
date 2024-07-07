57051f26f88c97d5f7ca49a2987518faf8577d6e
Rafi <rafifarhansabkiya@gmail.com>
2024-07-08 00:40:32 +0700

    modified token ExtractID

--- utils/token/token.go
+++ utils/token/token.go
@@ -1,7 +1,6 @@
 package token
 
 import (
-	"errors"
 	"final-project-rest-api/utils"
 	"fmt"
 	"strconv"
@@ -46,18 +45,25 @@ func TokenValid(c *gin.Context) error {
 }
 
 func ExtractToken(c *gin.Context) string {
-	bearerToken := c.GetHeader("Authorization")
-	strArr := strings.Split(bearerToken, " ")
-	if len(strArr) == 2 {
-		return strArr[1]
+	token := c.Query("token")
+	if token != "" {
+		return token
+	}
+	bearerToken := c.Request.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
 	}
 	return ""
 }
 
 func ExtractTokenID(c *gin.Context) (uint, error) {
+
 	tokenString := ExtractToken(c)
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte("your_secret_key"), nil // Replace "your_secret_key" with your actual secret key
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(API_SECRET), nil
 	})
 	if err != nil {
 		return 0, err
@@ -70,5 +76,5 @@ func ExtractTokenID(c *gin.Context) (uint, error) {
 		}
 		return uint(uid), nil
 	}
-	return 0, errors.New("invalid token")
+	return 0, nil
 }
